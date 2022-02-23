68fd2e99a10627409c5e33e6635aab1df23f00aa
SebasttianDaza <jsdaza64@misena.edu..co>
2022-02-23 10:02:21 -0500

    Add Component Team and corrected design
    I've added web component team and corrected design the web component Banner

---
+++ Components/Team/team.jsx
@@ -0,0 +1,17 @@
+import "./team.css";
+
+
+function Team () {
+    return (
+      <section className="Team">
+        <div className="teamChild">
+          <h4>Meet the team</h4>
+          <h3>We are chilled and a laidback team</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="teamChild2"></div>
+      </section>
+    );
+}
+
+export default Team
--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,16 @@ import Nav from '../Components/Nav/nav.jsx'
 import Banner from '../Components/Banner/banner.jsx'
 import Card from '../Components/Card/card.jsx'
 
+
+import Team from '../Components/Team/team.jsx'
+
 ReactDOM.render(
   <React.StrictMode>
     <Nav></Nav>
     <Banner></Banner>
     <Card></Card>
+
+    <Team></Team>
   </React.StrictMode>,
   document.getElementById('root')
 )
