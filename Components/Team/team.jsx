a3eb8ec10a371405c59aec32f2df96559200f431
SebasttianDaza <jsdaza64@misena.edu..co>
2022-02-23 14:41:21 -0500

    Complete design team
    I've completed design component design responsive

--- Components/Team/team.jsx
+++ Components/Team/team.jsx
@@ -9,7 +9,15 @@ function Team () {
           <h3>We are chilled and a laidback team</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
-        <div className="teamChild2"></div>
+        <div className="teamChild2">
+          <div>
+            <img src="../../assets/person3.png" alt="Image of Team"></img>
+          </div>
+          <aside>
+            <img src="../../assets/person1.png" alt="Image of Team"></img>
+            <img src="../../assets/person2.png" alt="Image of Team"></img>
+          </aside>
+        </div>
       </section>
     );
 }
