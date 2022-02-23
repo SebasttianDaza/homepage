4ea758083579c59d2c51a0522087b8fc330cd16d
SebasttianDaza <jsdaza64@misena.edu..co>
2022-02-23 18:29:14 -0500

    Add styles to footer

--- Components/Footer/footer.jsx
+++ Components/Footer/footer.jsx
@@ -5,10 +5,17 @@ function Footer () {
     return (
       <footer className="footer">
         <section className="content">
-          <div>
-            <Links></Links>
+          <div className="navFooter">
+            <Links id="link" className="links"></Links>
           </div>
-          <div>Hola </div>
+          <div className='navFooter1'>
+            <h4>Edie</h4>
+            <div>
+              <img src="../../assets/instagram.svg" alt="Image of Team"></img>
+              <img src="../../assets/linkedin.svg" alt="Image of Team"></img>
+              <img src="../../assets/twitter.svg" alt="Image of Team"></img>
+            </div>
+          </div>
           <div>Hola</div>
         </section>
         <p>created by Sebasttian Daza - devChallenges.io</p>
